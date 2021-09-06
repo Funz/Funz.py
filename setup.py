f2fad2a153a6b802414b8cf2601fd97c8f192ae9
Yann Richet <richetyann@gmail.com>
2021-09-06 17:56:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['numpy', 'py4j', ],
+    install_requires=['numpy', 'py4j', 'requests', ],
     include_package_data=True,
  )
