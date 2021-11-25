8f9e050274b1ff86db256ab067b781d563a8686b
yannrichet <richetyann@gmail.com>
2021-11-25 13:11:49 +0100

    up desc

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     scripts=['Funz/client.py','Funz/calculator.py','Funz/install.py'] ,
     author="Yann Richet",
     author_email="yann.richet@irsn.fr",
-    description="Funz Parametric Computing Environment",
+    description="Parametric Computing Environment",
     long_description="Binding to Funz <https://funz.github.io/> parametric computing environment, to emulate simulation model as a function. Also provide function to deal with Funz setup (eg. install plugin/binding to simulation software, algorithms, ...).",
     long_description_content_type="text/markdown",
     url="https://github.com/Funz/Funz.py",
